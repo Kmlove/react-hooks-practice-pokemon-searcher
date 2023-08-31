4ee26bd0db6ee6fc0f0a9d3608d740d6fa52e551
Kim Love <kimberlymlove15@gmail.com>
2023-08-30 23:06:48 -0400

    Searchbar complete

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -7,6 +7,7 @@ import { Container } from "semantic-ui-react";
 function PokemonPage() {
   const url = "http://localhost:3001/pokemon"
   const [pokemons, setPokemons] = useState([])
+  const [ searchName, setSearchName ] = useState("")
 
   useEffect(() => {
     fetch(url)
@@ -14,15 +15,23 @@ function PokemonPage() {
     .then(data => setPokemons(data))
   }, [])
 
+  function onUpdateSearchName(value){
+    setSearchName(value)
+  }
+
+  const pokemonsToDisplay = pokemons.filter(pokemon => {
+    return pokemon.name.toLowerCase().includes(searchName.toLowerCase())
+  })
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
       <PokemonForm />
       <br />
-      <Search />
+      <Search searchName={searchName} onUpdateSearchName={onUpdateSearchName} />
       <br />
-      <PokemonCollection pokemons={pokemons} />
+      <PokemonCollection pokemons={pokemonsToDisplay} />
     </Container>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-function Search() {
+function Search({searchName, onUpdateSearchName}) {
+  
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input 
+          className="prompt"
+          value={searchName} 
+          onChange={e => onUpdateSearchName(e.target.value)}
+        />
         <i className="search icon" />
       </div>
     </div>
